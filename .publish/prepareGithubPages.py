144b3592b8887899d93f87470eced766c7bf6871
s-blu <git@s-blu.de>
2022-09-01 22:39:50 +0200

    fix up tags and frontmatter (kind of)

--- .publish/prepareGithubPages.py
+++ .publish/prepareGithubPages.py
@@ -29,11 +29,18 @@ for (dirpath, dirnames, filenames) in os.walk(pagesDir):
                 filedata = re.sub(
                     r"---\s(%% === end of query page === %%)(.+)", '', filedata, flags=re.DOTALL)
 
-                # Change Callout syntax to adminition syntax
+                # Change Callout syntax to admonition syntax
                 filedata = re.sub(r"> \[\!(.+?)\][-+]? (.+)",
                                   r'!!! \1 "\2"', filedata)
+                # Remove lines that just contain a "> " if there's another "> something" following, since empty lines break the admonition syntax
+                filedata = re.sub(r">\s> (.+)", r'    \1', filedata)
                 # TODO this also converts standard cite blocks, would need to check if I have a match with the first one before this
                 filedata = re.sub(r"> (.+)", r'    \1', filedata)
+                # escape hashes from tags so they don't get rendered as h1
+                filedata = re.sub(r"(#)([^#\s]+)", r'\\#\2', filedata)
+                # extract description from frontmatter and replace frontmatter with it. Isn't the fanciest thing to do, need to think of something better.
+                filedata = re.sub(
+                    r"^\s*---\s*(description: .+)[\S\s]*?---", r'_\1_\n', filedata)
 
                 with open(filepath, 'w') as file:
                     file.write(filedata)
