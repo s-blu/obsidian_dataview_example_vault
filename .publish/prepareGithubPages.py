4f248f4d8ad682700e5f2d68d372aa962875ad91
s-blu <git@s-blu.de>
2022-09-02 09:53:00 +0200

    write tags to frontmatter and generate tagindex for navigation

--- .publish/prepareGithubPages.py
+++ .publish/prepareGithubPages.py
@@ -15,6 +15,20 @@ for dir in dirsToCopy:
 
 shutil.copy('README.md', f'{pagesDir}/index.md')
 shutil.copytree(f'.publish/assets', f'{pagesDir}/assets')
+shutil.copy('.publish/tagindex.md', f'{pagesDir}/tagindex.md')
+
+
+def moveTagsToFrontmatter(filedata):
+    tags = re.findall(r"[\s]#(dv[js]*/[^\^\#\`\s]+)", filedata)
+    if tags:
+        tagFrontmatter = '---\ntags:\n'
+        for tag in tags:
+            tagFrontmatter += f'  - {tag}\n'
+        tagFrontmatter += '---\n'
+        filedata = re.sub(r"[\s]#([^\^\#\s]+)", r'', filedata)
+        filedata = tagFrontmatter + filedata
+    return filedata
+
 
 for (dirpath, dirnames, filenames) in os.walk(pagesDir):
     for filename in filenames:
@@ -36,14 +50,21 @@ for (dirpath, dirnames, filenames) in os.walk(pagesDir):
                 filedata = re.sub(r">\s> (.+)", r'    \1', filedata)
                 # TODO this also converts standard cite blocks, would need to check if I have a match with the first one before this
                 filedata = re.sub(r"> (.+)", r'    \1', filedata)
-                # escape hashes from tags so they don't get rendered as h1
-                filedata = re.sub(r"(#)([^#\s]+)", r'\\#\2', filedata)
+
                 # extract description from frontmatter and replace frontmatter with it. Isn't the fanciest thing to do, need to think of something better.
                 filedata = re.sub(
-                    r"^\s*---\s*(description: .+)[\S\s]*?---", r'_\1_\n', filedata)
+                    r"^\s*---\s*description: (.+)[\S\s]*?---", r'!!! info "\1"\n', filedata)
+
+                # Add hint to Overview pages pointing to the tagindex
+                if ("31 Query Overviews" in filepath):
+                    filedata = '!!! warning "Overviews in Online Version"\n    The overview pages are not usable in online view. Please refer to the [[tagindex]].\n\n' + filedata
+
+                # Add tags to frontmatter
+                if ("20 Dataview Queries" in filepath):
+                    filedata = moveTagsToFrontmatter(filedata)
 
                 with open(filepath, 'w') as file:
                     file.write(filedata)
-        except:
+        except UnicodeDecodeError:
             print(
                 "WARN: Could not read file for replacing callout syntax, skipping: " + filepath)
---
+++ .publish/tagindex.md
@@ -0,0 +1,7 @@
+# Tag Index
+
+You're currently vieweing the **Online Version** of the vault. This comes with some limitations, first and foremost: Queries do not execute. Due to that, the Query Overviews are unusable, since they are using dataview themselves.
+
+To navigate the online version you can either use the **search bar** at the top or browse trough the page tags.
+
+[TAGS]
